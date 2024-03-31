225ff7f03d8e5cc459767592f41856a6c745bff0
Arm0101 <=>
2024-03-31 02:17:57 -0400

    tabla lexer

--- main.py
+++ main.py
@@ -1,28 +1,82 @@
 from src.lexer import Lexer
-
 nonzero_digits = '|'.join(str(n) for n in range(1, 10))
-zero = '|'.join(str(n) for n in range(0, 10))
+digits = '|'.join(str(n) for n in range(0, 10))
 letters = '|'.join(chr(n) for n in range(ord('a'), ord('z') + 1))
 cap_letters = '|'.join(chr(n) for n in range(ord('A'), ord('Z') + 1))
 letters = cap_letters + '|' + letters
-print('Non-zero digits:', nonzero_digits)
-print('Zero Dig', zero)
-print('Letters:', letters)
 
+no_first_zero = f'({nonzero_digits})*|(({nonzero_digits})(0)*)'
 lexer = Lexer([
-    ('num', f'({nonzero_digits})*|({nonzero_digits})*(.)({zero})*|(0)*(.)({zero})'),
-    ('for', 'for'),
-    ('foreach', 'foreach'),
+    ('num', f'{no_first_zero}|(({no_first_zero})*|(0))(.)({digits})*'),
+    ('_for', 'for'),
+    ('_foreach', 'foreach'),
+    ('_while', 'while'),
+    ('_range', 'range'),
+    ('_if', 'if'),
+    ('_else', 'else'),
+    ('_elif', 'elif'),
     ('space', ' *'),
-    ('equal', '='),
+
+    ('asig', '='),
+    ('des_operator', ':='),
+    ('mult', '\\*'),
+    ('div', '/'),
+    ('pow', '^'),
+    ('add', '+'),
+    ('sub', '-'),
+    ('lt', '<'),
+    ('le', '<='),
+    ('gt', '>'),
+    ('ge', '>='),
+    ('eq', '=='),
+    ('not_eq', '!='),
+    ('_and', '&'),
+    ('_or', '\\|'),
+    ('_not', '!'),
+    ('concatenation', '@'),
+    ('l_op', '\\||'),
+    ('exp', '\\**'),
+    ('_sqrt', 'sqrt'),
+    ('_sin', 'sin'),
+    ('_cos', 'cos'),
+    ('_exp', 'exp'),
+    ('_log', 'log'),
+    ('_rand', 'rand'),
+
+    ('Number', 'Number'),
+    ('Object', 'Object'),
+    ('Boolean', 'Boolean'),
     ('let', 'let'),
     ('in', 'in'),
-    ('f_print', 'print'),
+    ('_print', 'print'),
+    ('_size', 'size'),
+    ('_function', 'function'),
+    ('new', 'new'),
+    ('_type', 'type'),
+    ('_self', 'self'),
+    ('inherits','inherits'),
+    ('protocol', 'protocol'),
+    ('_extends', 'extends'),
+    ('_as', 'as'),
+    ('_is', 'is'),
+    ('arrow', '=>'),
+    ('colon', ':'),
     ('semicolon', ';'),
-    ('open_paran', '\\('),
-    ('close_paran', '\\)'),
+    ('comma', ','),
+    ('d_quotes', '"'),
+    #('esc_quotes', str("\"")),
+    # ('eol', str("\n")),
+    # ('tab', str("\t")),
+    ('o_parent', '\\('),
+    ('c_parent', '\\)'),
+    ('o_curly_brakes', '{'),
+    ('c_curly_brakes', '}'),
+    ('o_sq_bracket', '['),
+    ('c_sq_bracket', ']'),
     ('id', f'({letters})({letters}|0|{nonzero_digits})*')
 ], 'eof')
+
+
 
 # test
 
@@ -39,8 +93,6 @@ for i in text_list:
     tokens_l.append(lexer(i))
 
 for i in tokens_l:
+    print(i)
     for j in i:
         tokens.append(j)
-
-print(tokens)
-
